0897242482534894c8fd6eab1f7dddf102336ed2
Felipe Amorim <felipe21fab@gmail.com>
2023-08-21 20:45:02 -0300

    Create Seller findByDepartment

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
+
+import java.util.List;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -10,7 +13,14 @@ public class Main {
         System.out.println("----------Seller find by id ----------");
 
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
+
+        System.out.println("\n----------Seller find by department ----------");
+        Department department = new Department(2,null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+
+        for (Seller obj : list){
+            System.out.println(obj);
+        }
     }
 }
