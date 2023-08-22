e73d268052470a1042ac97616f8b76a3b54009da
Felipe Amorim <felipe21fab@gmail.com>
2023-08-21 21:12:03 -0300

    Create Seller update

--- src/Main.java
+++ src/Main.java
@@ -34,5 +34,11 @@ public class Main {
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! new id = " + newSeller.getId());
+
+        System.out.println("\n----------Seller update ----------");
+        seller = sellerDao.findById(1);
+        seller.setName("Rodrick Fernandez");
+        sellerDao.update(seller);
+
     }
 }
