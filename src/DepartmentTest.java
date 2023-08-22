0235013bcf1c6c5d41c195c5f884e821064cc9c2
Felipe Amorim <felipe21fab@gmail.com>
2023-08-21 21:43:16 -0300

    Finish DepartmentTest

--- src/DepartmentTest.java
+++ src/DepartmentTest.java
@@ -3,8 +3,14 @@ import model.dao.DepartmentDao;
 import model.dao.impl.DepartmentDaoJDBC;
 import model.entities.Department;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class DepartmentTest {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
         System.out.println("---------- Department insert ---------");
@@ -12,7 +18,24 @@ public class DepartmentTest {
         departmentDao.insert(department);
 
         System.out.println("---------- Department update ---------");
-        department =
+        department = departmentDao.findById(1);
+        department.setName("Music");
+        departmentDao.update(department);
 
+        System.out.println("---------- Department findById ---------");
+        department = departmentDao.findById(4);
+        System.out.println(department);
+
+        System.out.println("---------- Department findAll ---------");
+        List<Department> list = departmentDao.findAll();
+        for(Department obj : list){
+            System.out.println(obj);
+        }
+
+        System.out.println("---------- Department delete by id ---------");
+        System.out.println("Enter id for delete test: ");
+        int id = sc.nextInt();
+        departmentDao.deleteById(id);
+        System.out.println("Delete Completed");
     }
 }
