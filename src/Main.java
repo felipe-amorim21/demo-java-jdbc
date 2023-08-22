ac28b3eac8ad7ff063c8550f059677178fc12938
Felipe Amorim <felipe21fab@gmail.com>
2023-08-21 21:06:39 -0300

    Create Seller insert

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -28,5 +29,10 @@ public class Main {
         for (Seller obj : list){
             System.out.println(obj);
         }
+
+        System.out.println("\n----------Seller insert ----------");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! new id = " + newSeller.getId());
     }
 }
